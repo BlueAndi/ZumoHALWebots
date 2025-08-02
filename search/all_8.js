155e61f7fa83862bfdf4d49c83b8bd8f961f1505
BlueAndi <BlueAndi@users.noreply.github.com>
2025-08-02 16:26:19 +0000

    deploy: c28425be985cb9258ea5bdccd774602913f21ae2

---
+++ search/all_8.js
@@ -0,0 +1,25 @@
+var searchData=
+[
+  ['getaccelerationvalues_0',['getAccelerationValues',['../classIMU.html#a7647577516d68d8162a3374ac9bf8ac7',1,'IMU']]],
+  ['getcaliberrorinfo_1',['getCalibErrorInfo',['../classLineSensors.html#a2bf647de090ca10e18901968e225f6a3',1,'LineSensors']]],
+  ['getcountsandresetleft_2',['getCountsAndResetLeft',['../classEncoders.html#a9ca2ac39501420568de628ff474e7fa0',1,'Encoders']]],
+  ['getcountsandresetright_3',['getCountsAndResetRight',['../classEncoders.html#a55396f961f0fb0908596be053ceecb2b',1,'Encoders']]],
+  ['getcountsleft_4',['getCountsLeft',['../classEncoders.html#a0c965ee0082d447369cc3de2f7472594',1,'Encoders']]],
+  ['getcountsright_5',['getCountsRight',['../classEncoders.html#afe5c7ad7001d4aa24b8a9635529512bc',1,'Encoders']]],
+  ['getelapsedtimesincereset_6',['getElapsedTimeSinceReset',['../classSimTime.html#a1f397d1415367ad3d718f2a4ffe5ace0',1,'SimTime']]],
+  ['getleftspeed_7',['getLeftSpeed',['../classMotors.html#ad508717593e7be4030b913577783ef0b',1,'Motors']]],
+  ['getmagnetometervalues_8',['getMagnetometerValues',['../classIMU.html#a3bd0afaaebf0beaea62916c2fd2ff88c',1,'IMU']]],
+  ['getmaxspeed_9',['getmaxspeed',['../classMotors.html#aa318ec644fb62283b92d5c74176a6595',1,'Motors::getMaxSpeed()'],['../classSettings.html#aeb17f580fe3addbfe3216cc4658797ff',1,'Settings::getMaxSpeed()']]],
+  ['getnumbrightnesslevels_10',['getNumBrightnessLevels',['../classProximitySensors.html#a8bfdd9643526614b535d630f0265fcd9',1,'ProximitySensors']]],
+  ['getnumlinesensors_11',['getNumLineSensors',['../classLineSensors.html#a830b67f91c13c891a86b8eb78b9f6bc5',1,'LineSensors']]],
+  ['getnumsensors_12',['getNumSensors',['../classProximitySensors.html#a04cb2e33610373b97557a0d8b4eadde4',1,'ProximitySensors']]],
+  ['getpath_13',['getPath',['../classSettings.html#a5e6fbc18046f0c6e5b11eff30bde0af9',1,'Settings']]],
+  ['getpressedbuttons_14',['getPressedButtons',['../classKeyboard.html#a1b41f3e25e1746a9fda6481b887fd96c',1,'Keyboard']]],
+  ['getrightspeed_15',['getRightSpeed',['../classMotors.html#a9ec783895562a83ad75d7515985014ce',1,'Motors']]],
+  ['getsensorvaluemax_16',['getSensorValueMax',['../classLineSensors.html#a24b47479ac5f1be14fe21a45983b04d2',1,'LineSensors']]],
+  ['getsensorvalues_17',['getSensorValues',['../classLineSensors.html#a14e63fdf77c082bb89b9ecf8ddb0683d',1,'LineSensors']]],
+  ['gettimestep_18',['getTimeStep',['../classSimTime.html#a6a076a66a68d07de8b7cfce14f27563a',1,'SimTime']]],
+  ['getturnrates_19',['getTurnRates',['../classIMU.html#aeadcef5ba284f183ca276ea48640fee8',1,'IMU']]],
+  ['gotoxy_20',['gotoXY',['../classDisplay.html#a5270ccb4d29dd7e1d7c8e75cdcd364d3',1,'Display']]],
+  ['gyrodataready_21',['gyroDataReady',['../classIMU.html#a9dc527b960450948c7f4f79aebdc5694',1,'IMU']]]
+];
---
+++ search/all_d.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['magnetometerdataready_0',['magnetometerDataReady',['../classIMU.html#a9aaeb80d530926dec60373813d8577cc',1,'IMU']]],
+  ['max_5fvolume_1',['MAX_VOLUME',['../classBuzzer.html#a359cadc5eb92b2a1f572b24c49758354',1,'Buzzer']]],
+  ['motors_2',['motors',['../classMotors.html',1,'Motors'],['../classMotors.html#a17822e6e475ad9885545af7b97fc32a4',1,'Motors::Motors()']]]
+];
